064f1b43035e0282f682c94bda4000701d1582c8
Dominic Torruellas <dtorruellas@gmail.com>
2017-12-13 12:35:28 -0700

    messing around

--- chap05/methods/src/main.rs
+++ chap05/methods/src/main.rs
@@ -25,10 +25,14 @@ fn main() {
 
     let square1 = Rectangle::square(32);
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    // println!(
+    //     "The area of the rectangle is {} square pixels.",
+    //     rect1.area()
+    // );
+
+    println!("Area of rect1: {}", rect1.area());
+    println!("Area of rect2: {}", rect2.area());
+    println!("Area of rect3: {}", rect3.area());
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
